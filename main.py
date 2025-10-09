fe3d9c3194141564aa414deb65a96791abcccb59
mbsomeday <137542994@qq.com>
2025-10-09 10:06:39 +0100

    a

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_args():
     parser.add_argument('--batch_size', default=4, type=int)
 
     # train
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--min_train_epoch', type=int, default=30)
     parser.add_argument('--weight_decay', type=float, default=1e-5)
     parser.add_argument('--lr', type=float, default=1e-4)
