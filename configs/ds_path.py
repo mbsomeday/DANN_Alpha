008bbe6b30c804817da88136ed8a0ce2a72ca06d
mbsomeday <137542994@qq.com>
2025-10-21 14:32:15 +0100

    a

--- configs/ds_path.py
+++ configs/ds_path.py
@@ -73,9 +73,9 @@ LOCAL = {
 
 KAGGLE = {
     'Stage6_org': {
-        'D1': r'/kaggle/input/stage6-dataset-ecp',
-        'D2': r'/kaggle/input/stage6-dataset-citypersons',
-        'D3': r'/kaggle/input/stage6-dataset-bdd100k',
+        'D1': r'/kaggle/input/stage6-datasets-organize/ECP3000',
+        'D2': r'/kaggle/input/stage6-datasets-organize/CityPersons3000',
+        'D3': r'/kaggle/input/stage6-datasets-organize/bdd100k3000',
     },
 
     'Stage6_fade': {
