9b34f90a437fe759f6b1736604fdafdb90461718
mbsomeday <137542994@qq.com>
2025-10-27 21:03:24 +0000

    a

--- experiments/DANN_method.py
+++ experiments/DANN_method.py
@@ -95,7 +95,7 @@ class DANN_Trainer(object):
 
         y_true = []
         y_pred = []
-        val_loss = 0
+        val_loss = 0.0
 
         with torch.no_grad():
             for batch_idx, data_dict in enumerate(tqdm(data_loader, desc=f'Epoch {epoch} val')):
@@ -112,7 +112,6 @@ class DANN_Trainer(object):
         val_bc = balanced_accuracy_score(y_true, y_pred)
         cm = confusion_matrix(y_true=y_true, y_pred=y_pred, labels=range(2))
 
-        print(f'Val loss {val_loss:.6f}, val_bc:{val_bc:.4f}')
         # print(f'CM on validation set:\n{cm}')
 
         val_epoch_info = {
@@ -199,12 +198,72 @@ class DANN_Trainer(object):
         lr = self.optimizer.param_groups[0]['lr']
         print('learning rate %.7f -> %.7f' % (old_lr, lr))
 
+    def train_one_epoch(self, epoch, min_len):
+        self.label_model.train()
+        self.feature_model.train()
+        self.domain_model.train()
+
+        loss_val = 0.0
+        y_true = []
+        y_pred = []
+
+        for batch_idx, (source_dict, target_dict) in tqdm(enumerate(zip(self.s_train_loader, self.t_train_loader)),
+                                                          total=len(self.s_train_loader),
+                                                          desc=f'Epoch {epoch} train'):
+            # 调节domain classifier的alpha
+            # total_iters += 1
+            alpha = adjust_alpha(batch_idx, epoch, min_len, self.max_epochs)
+
+            # 加载数据
+            source, s_labels = source_dict['image'].to(DEVICE), source_dict['ped_label'].to(DEVICE)
+            target, _ = target_dict['image'].to(DEVICE), target_dict['ped_label'].to(DEVICE)
+
+            # label classifier
+            s_feature = self.feature_model(source)
+            s_out = self.label_model(s_feature)
+            s_preds = torch.argmax(s_out, dim=1)
+
+            t_feature = self.feature_model(target)
+            t_out = self.label_model(t_feature)
+
+            # domain classifier
+            s_domain_out = self.domain_model(s_feature, alpha=alpha)
+            t_domain_out = self.domain_model(t_feature, alpha=alpha)
+
+            # 两个classifier都用交叉熵损失
+            real_label = torch.ones(size=(source.shape[0],), dtype=torch.long).to(DEVICE)
+            fake_label = torch.zeros(size=(target.shape[0],), dtype=torch.long).to(DEVICE)
+            s_domain_err = self.ce(s_domain_out, real_label)
+            t_domain_err = self.ce(t_domain_out, fake_label)
+
+            domain_loss = s_domain_err + t_domain_err
+            s_label_loss = self.ce(s_out, s_labels)
+            loss = s_label_loss + domain_loss
+            loss_val += loss.item()
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # 记录在source domain上的accuracy
+            y_true.extend(s_labels.cpu().numpy())
+            y_pred.extend(s_preds.cpu().numpy())
+
+        train_bc = balanced_accuracy_score(y_true, y_pred)
+
+        train_epoch_info = {
+            'train_bc': train_bc,
+            'loss': loss_val
+        }
+
+        return train_epoch_info
+
 
     def train(self):
         s_iter_per_epoch = len(self.s_train_loader)
         t_iter_per_epoch = len(self.t_train_loader)
         min_len = min(s_iter_per_epoch, t_iter_per_epoch)
-        total_iters = 0
+        # total_iters = 0
 
         print("Source iters per epoch: %d" % (s_iter_per_epoch))
         print("Target iters per epoch: %d" % (t_iter_per_epoch))
@@ -217,47 +276,12 @@ class DANN_Trainer(object):
 
 
         for EPOCH in range(self.max_epochs):
-            self.label_model.train()
-            self.feature_model.train()
-            self.domain_model.train()
 
-            for batch_idx, (source_dict, target_dict) in tqdm(enumerate(zip(self.s_train_loader, self.t_train_loader)),
-                                                              total=len(self.s_train_loader),
-                                                              desc=f'Epoch {(EPOCH+1)} train'):
-                # 调节domain classifier的alpha
-                total_iters += 1
-                alpha = adjust_alpha(batch_idx, (EPOCH+1), min_len, self.max_epochs)
-
-                # 加载数据
-                source, s_labels = source_dict['image'].to(DEVICE), source_dict['ped_label'].to(DEVICE)
-                target, _ = target_dict['image'].to(DEVICE), target_dict['ped_label'].to(DEVICE)
-
-                # label classifier
-                s_feature = self.feature_model(source)
-                s_out = self.label_model(s_feature)
-
-                t_feature = self.feature_model(target)
-                t_out = self.label_model(t_feature)
-
-                # domain classifier
-                s_domain_out = self.domain_model(s_feature, alpha=alpha)
-                t_domain_out = self.domain_model(t_feature, alpha=alpha)
-
-                # 两个classifier都用交叉熵损失
-                real_label = torch.ones(size=(source.shape[0], ), dtype=torch.long).to(DEVICE)
-                fake_label = torch.zeros(size=(target.shape[0], ), dtype=torch.long).to(DEVICE)
-                s_domain_err = self.ce(s_domain_out, real_label)
-                t_domain_err = self.ce(t_domain_out, fake_label)
-
-                domain_loss = s_domain_err + t_domain_err
-                s_label_loss = self.ce(s_out, s_labels)
-                loss = s_label_loss + domain_loss
-
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-
+            train_info = self.train_one_epoch(EPOCH+1, min_len=min_len)
             val_info = self.val_on_epoch_end(self.t_val_loader, epoch=EPOCH+1)
+
+            print(f'Train loss {train_info["loss"]:.6f}, train_bc:{train_info["train_bc"]:.4f}')
+            print(f'Val loss {val_info["loss"]:.6f}, val_bc:{val_info["val_bc"]:.4f}')
 
             self.update_learning_rate(EPOCH+1)
 
